285efe070c8ef087d8a4dd3f5f7dbe6ba5686b28
adrono7 <adam.noack@gmail.com>
2025-08-20 15:14:30 -0400

    three done

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,8 +1,29 @@
 import requests
+import json
 
 def emotion_detector(text_to_analyse):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     myobj = { "raw_document": { "text": text_to_analyse } }
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     response = requests.post(url, json = myobj, headers=header)
-    return response.text
+    formatted_response = json.loads(response.text)
+
+    anger = formatted_response['emotionPredictions'][0]['emotion']['anger']
+    disgust = formatted_response['emotionPredictions'][0]['emotion']['disgust']
+    fear = formatted_response['emotionPredictions'][0]['emotion']['fear']
+    joy = formatted_response['emotionPredictions'][0]['emotion']['joy']
+    sadness = formatted_response['emotionPredictions'][0]['emotion']['sadness']
+
+    emotion_list = [anger, disgust, fear, joy, sadness]
+    dominant_emotion_index = emotion_list. index(max(emotion_list))
+    emotion_keys = ["anger", "disgust", "fear", "joy", "sadness"]
+    dominant_emotion_result = emotion_keys[dominant_emotion_index]
+
+    return {
+        'anger' : anger,
+        'disgust': disgust,
+        'fear': fear,
+        'joy': joy,
+        'sadness': sadness,
+        'dominant_emotion': dominant_emotion_result
+    }
